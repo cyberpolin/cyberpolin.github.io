3f204e2d4dab362c02b238eba201e1174faa3326
Carlos Gonzalez <cyberpolin@gmail.com>
2017-12-17 02:38:41 -0600

    Add Images component

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import {Flex, Col, Row} from './components/grid'
+import Photo from './components/photo'
 
 import styled from 'styled-components'
 
 const ImageContainer = styled(Flex)`
     flex: 3;
+    text-align: center;
+    align-items: center;
+    background: url(/img/profile-bg.jpg);
+    background-size: cover;
+    display: flex;
   @media (max-width: 600px){
     flex: 8;
   }
@@ -27,10 +33,14 @@ class App extends Component {
       <Wrapper>
         <Col flex={5}>
           <ImageContainer>
-            image
+            <Photo />
           </ImageContainer>
           <Menu >
-            menu
+            <nav>
+              <a href='#'>Profile</a>
+              <a href='#'>Portfolio</a>
+              <a href='#'>Hire</a>
+            </nav>
           </Menu>
         </Col>
         <Col flex={13} style={{background: 'blue'}} >
