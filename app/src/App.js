c6aa26c51df7445cbde55c885ac01d9a9a50a3bf
Carlos Gonzalez <cyberpolin@gmail.com>
2017-12-17 00:18:07 -0600

    Basic layout done

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,22 @@ import {Flex} from './components/flex'
 class App extends Component {
   render () {
     return (
-      <Flex row />
+      <Flex row style={{height: '100vh'}}>
+        <Flex column flex={5} style={{background: 'red'}}>
+          <Flex flex={3} style={{background: 'green'}}>
+            image
+          </Flex>
+          <Flex flex={8} style={{background: 'red'}}>
+            menu
+          </Flex>
+        </Flex>
+        <Flex column flex={13} style={{background: 'blue'}} >
+          <Flex flex={9} style={{background: 'gray'}} />
+          <Flex flex={1} style={{background: 'blue'}} />
+
+          Content
+        </Flex>
+      </Flex>
     )
   }
 }
--- app/src/components/flex.js
+++ app/src/components/flex.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
 
 const Flex = styled.div`
-  display: ${({row}) => row && 'flex'};
+  display: ${({row, column}) => row || column ? 'flex' : null};
   flex: ${({flex}) => flex || 1};
+  flex-direction: ${({column}) => column && 'column'}
 `
 
 export {Flex}
